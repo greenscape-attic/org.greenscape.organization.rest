84cfe5d9464a0790784401456ff78f27f57b9d86
smsajid <smsajid786@gmail.com>
2015-03-17 14:55:25 +0530

    https://github.com/greenscape/greenscape-portal/issues/8 - fix build

--- src/org/greenscape/organization/rest/OrganizationModelParam.java
+++ src/org/greenscape/organization/rest/OrganizationModelParam.java
@@ -1,6 +1,13 @@
 package org.greenscape.organization.rest;
 
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+
 import org.greenscape.organization.OrganizationModel;
+import org.greenscape.persistence.AuditedModel;
+import org.greenscape.persistence.DocumentModel;
 
 public class OrganizationModelParam implements OrganizationModel {
 
@@ -8,7 +15,7 @@ public class OrganizationModelParam implements OrganizationModel {
 	 * TODO: primitive type used because of swagger bug. using wrapper types
 	 * causes infinite recursion.
 	 */
-	private long organizationId;
+	private String organizationId;
 	private String name;
 	private String homeURL;
 	private long logoId;
@@ -16,12 +23,12 @@ public class OrganizationModelParam implements OrganizationModel {
 	private boolean isActive;
 
 	@Override
-	public Long getOrganizationId() {
+	public String getOrganizationId() {
 		return organizationId;
 	}
 
 	@Override
-	public OrganizationModelParam setOrganizationId(Long organizationId) {
+	public OrganizationModelParam setOrganizationId(String organizationId) {
 		this.organizationId = organizationId;
 		return this;
 	}
@@ -79,6 +86,96 @@ public class OrganizationModelParam implements OrganizationModel {
 	public OrganizationModelParam setActive(Boolean isActive) {
 		this.isActive = isActive;
 		return this;
+	}
+
+	@Override
+	public Object getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DocumentModel setId(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getModelId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DocumentModel setModelId(String modelId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getProperty(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T getProperty(Class<T> clazz, String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DocumentModel setProperty(String name, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPropertySize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<String, Object> getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<String> getPropertyNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Object> getPropertyValues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getCreatedDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AuditedModel setCreatedDate(Date date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getModifiedDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AuditedModel setModifiedDate(Date date) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
